1dafb585e8750dbbff4c225d9310bdf20e0a738a
glebk <gleb.kor888@gmail.com>
2026-03-30 11:31:04 +0100

    Safety

--- kittybot/src/main/kotlin/org/bezsahara/kittybot/telegram/classes/core/update/UpdateKind.kt
+++ kittybot/src/main/kotlin/org/bezsahara/kittybot/telegram/classes/core/update/UpdateKind.kt
@@ -1,5 +1,6 @@
 package org.bezsahara.kittybot.telegram.classes.core.update
 
+
 /**
  * Metadata about types of Update. It is always and only applied to
  * companion objects of Update subclasses. So to specify update kind
@@ -12,4 +13,16 @@ sealed class UpdateKind<T>(
     @JvmField val ordinal: Int,
     @JvmField val clazz: Class<T>,
     @JvmField val name: String
-)
+) {
+    init {
+        val put = sanityCheck[ordinal]
+        require(put == null) {
+            "Apparently some updates are using same ordinals. 1: ${put!!.simpleName} 2: ${clazz.simpleName}"
+        }
+        sanityCheck[ordinal] = clazz
+    }
+
+    companion object {
+        private val sanityCheck = arrayOfNulls<Class<*>>(25)
+    }
+}
